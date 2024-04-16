2d87c2610e16b434b5c62295469171ed296b1f02
AbduSalam <98763674+AbduSalam08@users.noreply.github.com>
2024-04-16 13:03:03 +0530

    Main Initial Push

---
+++ src/webparts/readifyEmMain/components/IReadifyEmMainProps.ts
@@ -0,0 +1,7 @@
+export interface IReadifyEmMainProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/readifyEmMain/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IReadifyEmMainWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'ReadifyEmMainWebPartStrings' {
+  const strings: IReadifyEmMainWebPartStrings;
+  export = strings;
+}
