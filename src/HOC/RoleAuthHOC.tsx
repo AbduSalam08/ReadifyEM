481866b49770571bed26f55d0dc928616f9a1f91
MANIKANDAN <manidhiyamech07167@gmail.com>
2024-07-15 15:30:49 +0530

    15/07/24 mani's changes

--- src/HOC/RoleAuthHOC.tsx
+++ src/HOC/RoleAuthHOC.tsx
@@ -1,32 +1,40 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable no-constant-condition */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import AdminLayout from "../layouts/AdminLayout/AdminLayout";
 import UserLayout from "../layouts/UserLayout/UserLayout";
+import { useDispatch, useSelector } from "react-redux";
+import { RoleAuth } from "../services/RoleAUTH/RoleAUTHServices";
 
 const RoleAuthorizationHOC = (): JSX.Element => {
-  // const mainContext: any = useSelector(
-  //   (state: any) => state.MainSPContext.value
-  // );
-  const [currentUser, setCurrentUser] = useState({
-    admin: false,
-    email: "abdulsalam@gmail.com",
-  });
+  // redux dispatcher
+  const dispatch = useDispatch();
+  const mainContext: any = useSelector(
+    (state: any) => state.MainSPContext.value
+  );
+
+  const currentUserDetails: any = useSelector(
+    (state: any) => state.MainSPContext.currentUserDetails
+  );
+
+  const getRoleAuth = async (): Promise<any> => {
+    const currentUserDetails: any = {
+      userName: mainContext?._pageContext?._user?.displayName,
+      Email: mainContext?._pageContext?._user?.email,
+    };
+    await RoleAuth(currentUserDetails, dispatch);
+  };
 
   useEffect(() => {
-    if (true) {
-      setCurrentUser((prev: any) => ({
-        ...prev,
-        admin: true,
-      }));
-    } else {
-      setCurrentUser((prev: any) => ({
-        ...prev,
-        admin: false,
-      }));
-    }
+    getRoleAuth();
   }, []);
-  return currentUser.admin ? <AdminLayout /> : <UserLayout />;
+
+  return currentUserDetails?.role === "Admin" ? (
+    <AdminLayout />
+  ) : (
+    <UserLayout />
+  );
 };
 
 export default RoleAuthorizationHOC;
