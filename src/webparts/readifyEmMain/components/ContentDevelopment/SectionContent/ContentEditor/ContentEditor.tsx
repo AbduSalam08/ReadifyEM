6a9ce2d10730eae7733ef7047b0103bd81b2c441
MANIKANDAN <manidhiyamech07167@gmail.com>
2024-09-10 23:23:10 +0530

    10/09 manis changes p3

--- src/webparts/readifyEmMain/components/ContentDevelopment/SectionContent/ContentEditor/ContentEditor.tsx
+++ src/webparts/readifyEmMain/components/ContentDevelopment/SectionContent/ContentEditor/ContentEditor.tsx
@@ -3,7 +3,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 /* eslint-disable @typescript-eslint/no-floating-promises */
-import * as React from "react";
 // import { useState } from "react";
 import ReactQuill, { Quill } from "react-quill";
 import "./ContentEditor.css";
@@ -34,122 +33,129 @@ interface EditorProps {
   readOnly?: boolean;
 }
 import "react-quill/dist/quill.bubble.css";
-import { useRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
-const ContentEditor: React.FC<EditorProps> = ({
-  placeholder,
-  setEditorHtml,
-  editorHtmlValue,
-  readOnly,
-}) => {
-  const quillRef = useRef<any>(null);
-  const handleChange = async (
-    html: string,
-    delta: any,
-    source: any,
-    editor: any
-  ) => {
-    console.log("html", html);
-    console.log("delta", delta);
-    console.log("source", source);
-    console.log("editor", editor);
-    debugger;
-    const quill = quillRef.current.getEditor();
+const ContentEditor = forwardRef<HTMLDivElement, EditorProps>(
+  ({ placeholder, setEditorHtml, editorHtmlValue, readOnly }, ref) => {
+    const quillRef = useRef<any>(null);
 
-    const currentContents = quill.getContents();
-    let cumulativeIndex = 0;
+    // Expose the Quill editor instance via the ref
+    useImperativeHandle(ref, (): any => ({
+      focus() {
+        if (quillRef.current) {
+          quillRef.current.getEditor().focus();
+        }
+      },
+    }));
 
-    for (let i = 0; i < currentContents.ops.length; i++) {
-      const op = currentContents.ops[i];
-      if (op.insert && op.insert.image) {
-        const imgSrc = op.insert.image;
-        const response = await fetch(imgSrc);
-        const blob = await response.blob();
-        if (blob.size > 1 * 1024 * 1024 || blob.size < 500 * 1024) {
-          // 1MB limit
-          // Remove the large image from the editor's content
-          quill.deleteText(cumulativeIndex, 1);
-          alert(
-            "Image size is either too large (over 1MB) or too small (below 500KB) and has been removed."
-          );
-          cumulativeIndex -= 1;
-        } else {
-          // Get the image element and set its size to 300px by 300px
-          const imageElement = quill.container.querySelector(
-            `img[src="${imgSrc}"]`
-          );
-          if (imageElement) {
-            imageElement.style.width = "400px";
-            imageElement.style.height = "400px";
+    const handleChange = async (
+      html: string,
+      delta: any,
+      source: any,
+      editor: any
+    ) => {
+      console.log("html", html);
+      console.log("delta", delta);
+      console.log("source", source);
+      console.log("editor", editor);
+      debugger;
+      const quill = quillRef.current.getEditor();
+
+      const currentContents = quill.getContents();
+      let cumulativeIndex = 0;
+
+      for (let i = 0; i < currentContents.ops.length; i++) {
+        const op = currentContents.ops[i];
+        if (op.insert && op.insert.image) {
+          const imgSrc = op.insert.image;
+          const response = await fetch(imgSrc);
+          const blob = await response.blob();
+          if (blob.size > 1 * 1024 * 1024 || blob.size < 500 * 1024) {
+            // 1MB limit
+            // Remove the large image from the editor's content
+            quill.deleteText(cumulativeIndex, 1);
+            alert(
+              "Image size is either too large (over 1MB) or too small (below 500KB) and has been removed."
+            );
+            cumulativeIndex -= 1;
+          } else {
+            // Get the image element and set its size to 300px by 300px
+            const imageElement = quill.container.querySelector(
+              `img[src="${imgSrc}"]`
+            );
+            if (imageElement) {
+              imageElement.style.width = "400px";
+              imageElement.style.height = "400px";
+            }
           }
         }
+
+        // Update the cumulative index for each op
+        cumulativeIndex += op.insert
+          ? typeof op.insert === "string"
+            ? op.insert.length
+            : 1
+          : 0;
       }
 
-      // Update the cumulative index for each op
-      cumulativeIndex += op.insert
-        ? typeof op.insert === "string"
-          ? op.insert.length
-          : 1
-        : 0;
-    }
-
-    setEditorHtml(editor.getHTML());
-    console.log("editor", editor.getHTML());
-    // setEditorHtml(editor.getHTML());
-    // if (html) {
-    //   setEditorHtml(html);
-    // }
-  };
-  const modules = {
-    toolbar: [
-      ["bold", "italic", "underline"],
-      ["image"],
-      // ["link"],
-      [{ list: "ordered" }, { list: "bullet" }],
-      [{ align: [] }],
-      [{ header: [1, 2, 3, 4, 5, 6, false] }],
-      [
-        { color: ["black", "red", "orange", "yellow", "green", "blue"] },
-        { background: ["black", "red", "orange", "yellow", "green", "blue"] },
+      setEditorHtml(editor.getHTML());
+      console.log("editor", editor.getHTML());
+      // setEditorHtml(editor.getHTML());
+      // if (html) {
+      //   setEditorHtml(html);
+      // }
+    };
+    const modules = {
+      toolbar: [
+        ["bold", "italic", "underline"],
+        ["image"],
+        // ["link"],
+        [{ list: "ordered" }, { list: "bullet" }],
+        [{ align: [] }],
+        [{ header: [1, 2, 3, 4, 5, 6, false] }],
+        [
+          { color: ["black", "red", "orange", "yellow", "green", "blue"] },
+          { background: ["black", "red", "orange", "yellow", "green", "blue"] },
+        ],
+        ["clean"],
       ],
-      ["clean"],
-    ],
-    // clipboard: {
-    //   matchVisual: false,
-    // },
-    // imageResize: {
-    //   parchment: Quill.import("parchment"),
-    //   modules: ["Resize", "DisplaySize"],
-    // },
-  };
+      // clipboard: {
+      //   matchVisual: false,
+      // },
+      // imageResize: {
+      //   parchment: Quill.import("parchment"),
+      //   modules: ["Resize", "DisplaySize"],
+      // },
+    };
 
-  const formats = [
-    "bold",
-    "italic",
-    "underline",
-    "image",
-    "color",
-    "background",
-    "header",
-    "list",
-    // "link",
-    "align",
-  ];
+    const formats = [
+      "bold",
+      "italic",
+      "underline",
+      "image",
+      "color",
+      "background",
+      "header",
+      "list",
+      // "link",
+      "align",
+    ];
 
-  return (
-    <ReactQuill
-      ref={quillRef}
-      readOnly={readOnly}
-      theme="bubble"
-      className="quillWrap"
-      onChange={handleChange}
-      value={editorHtmlValue}
-      modules={modules}
-      formats={formats}
-      bounds={`[data-text-editor="form-editor"]`}
-      placeholder={placeholder}
-    />
-  );
-};
+    return (
+      <ReactQuill
+        ref={quillRef}
+        readOnly={readOnly}
+        theme="bubble"
+        className="quillWrap"
+        onChange={handleChange}
+        value={editorHtmlValue}
+        modules={modules}
+        formats={formats}
+        bounds={`[data-text-editor="form-editor"]`}
+        placeholder={placeholder}
+      />
+    );
+  }
+);
 
 export default ContentEditor;
--- src/webparts/readifyEmMain/components/Table/PDFServiceTemplate/PDFServiceTemplate.tsx
+++ src/webparts/readifyEmMain/components/Table/PDFServiceTemplate/PDFServiceTemplate.tsx
@@ -4,20 +4,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import { memo, useEffect, useState } from "react";
-// import DefaultButton from "../../common/Buttons/DefaultButton";
 import styles from "./PDFServiceTemplate.module.scss";
 import "./PDFServiceTemplate.css";
-// import jsPDF from "jspdf";
-// import pdfMake from "pdfmake";
-// import pdfFonts from "pdfmake/build/vfs_fonts";
-// import pdfMake from "pdfmake/build/pdfmake";
-// import pdfFonts from "pdfmake/build/vfs_fonts";
-// import htmlToPdfmake from "html-to-pdfmake";
-// import SpServices from "../../../../../services/SPServices/SpServices";
 import CircularSpinner from "../../common/AppLoader/CircularSpinner";
 import { getDocumentRelatedSections } from "../../../../../services/PDFServices/PDFServices";
-// import DefaultButton from "../../common/Buttons/DefaultButton";
-// import html2pdf from "html2pdf.js";
 interface Iprops {
   documentId: number;
 }
@@ -30,64 +20,44 @@ const PDFServiceTemplate: React.FC<Iprops> = ({ documentId }) => {
   const [loader, setLoader] = useState<boolean>(false);
   console.log(allSectionContent);
 
-  // const readTextFileFromTXT = (data: any, index: number): void => {
-  //   SpServices.SPReadAttachments({
-  //     ListName: "SectionDetails",
-  //     ListID: data.ID,
-  //     AttachmentName: data?.FileName,
-  //   })
-  //     .then((res: any) => {
-  //       const parsedValue: any = JSON.parse(res);
-  //       const sectionDetails = {
-  //         text: data.sectionName,
-  //         sectionOrder: data.sectionOrder,
-  //         value: parsedValue,
-  //       };
-  //       setAllSectionContent((prev: any) => {
-  //         // Add the new sectionDetails to the previous state
-  //         const updatedSections = [...prev, sectionDetails];
-
-  //         // Sort the updatedSections array by the "sectionOrder" key
-  //         updatedSections.sort((a, b) => {
-  //           return parseInt(a.sectionOrder) - parseInt(b.sectionOrder);
-  //         });
-
-  //         // Return the sorted array to update the state
-  //         return updatedSections;
-  //       });
-  //       if (index + 1 === AllSectionsAttachments.length) {
-  //         setLoader(false);
-  //       }
-  //     })
-  //     .catch((err: any) => {
-  //       console.log("err: ", err);
-  //     });
-  // };
-
-  // const readSectionAttachments = (): any => {
-  //   if (AllSectionsAttachments.length !== 0) {
-  //     AllSectionsAttachments.forEach((item: any, index: number) => {
-  //       const filteredItem: any = item?.filter(
-  //         (item: any) => item?.FileName === "Sample.txt"
-  //       );
-  //       if (filteredItem.length > 0) {
-  //         readTextFileFromTXT(filteredItem[0], index);
-  //       } else {
-  //         // setSectionLoader(false);
-  //       }
-  //     });
-  //   } else {
-  //     // setLoader(false);
-  //   }
-  // };
   useEffect(() => {
     getDocumentRelatedSections(documentId, setAllSectionContent, setLoader);
   }, []);
 
-  // useEffect(() => {
-  //   setLoader(true);
-  //   readSectionAttachments();
-  // }, [AllSectionsAttachments]);
+  // Function to render points recursively
+  const renderPoints = (
+    arr: any[],
+    parentPath: number[] = []
+  ): JSX.Element[] => {
+    return arr.map((point, index) => {
+      // Calculate the current path for this point (parent path + current index)
+      const currentPath = [...parentPath, index];
+
+      // Determine the indent level based on the length of the parent path
+      const indentLevel = parentPath.length;
+      const marginLeft = indentLevel * 31; // Adjust this value for appropriate spacing
+      const nestedStyle: React.CSSProperties = {
+        marginLeft: `${marginLeft}px`,
+        display: "flex",
+        alignItems: "center",
+        paddingBottom: "13px",
+      };
+
+      return (
+        <div key={point.text}>
+          <div className={styles.listItem} style={nestedStyle} key={index}>
+            <span style={{ marginRight: "10px" }}>{point.text}</span>
+            <span
+              dangerouslySetInnerHTML={{
+                __html: point.value,
+              }}
+            />
+          </div>
+          {point.childs.length > 0 && renderPoints(point.childs, currentPath)}
+        </div>
+      );
+    });
+  };
 
   return (
     <>
@@ -119,35 +89,37 @@ const PDFServiceTemplate: React.FC<Iprops> = ({ documentId }) => {
                 {typeof obj.value === "string" ? (
                   <div dangerouslySetInnerHTML={{ __html: obj.value }} />
                 ) : (
-                  obj.value.map((list: any, index: number) => {
-                    const indent = list.text.split(".").length - 1;
-                    console.log(indent);
-                    const marginLeft = (indent + 1 - 1) * 26;
-                    const nestedStyle: React.CSSProperties = {
-                      marginLeft: `${marginLeft}px`,
-                      display: "flex",
-                      alignItems: "center",
-                      paddingBottom: "13px",
-                    };
-                    return (
-                      list.value !== "" && (
-                        <div
-                          className={styles.listItem}
-                          style={nestedStyle}
-                          key={index}
-                        >
-                          <span style={{ marginRight: "10px" }}>
-                            {list.text}
-                          </span>
-                          <span
-                            dangerouslySetInnerHTML={{
-                              __html: list.value,
-                            }}
-                          />
-                        </div>
-                      )
-                    );
-                  })
+                  // obj.value.map((list: any, index: number) => {
+                  //   const indent = list.text.split(".").length - 1;
+                  //   console.log(indent);
+                  //   const marginLeft = (indent + 1 - 1) * 26;
+                  //   const nestedStyle: React.CSSProperties = {
+                  //     marginLeft: `${marginLeft}px`,
+                  //     display: "flex",
+                  //     alignItems: "center",
+                  //     paddingBottom: "13px",
+                  //   };
+                  //   return (
+                  //     list.value !== "" && (
+                  //       <div
+                  //         className={styles.listItem}
+                  //         style={nestedStyle}
+                  //         key={index}
+                  //       >
+                  //         <span style={{ marginRight: "10px" }}>
+                  //           {list.text}
+                  //         </span>
+                  //         <span
+                  //           dangerouslySetInnerHTML={{
+                  //             __html: list.value,
+                  //           }}
+                  //         />
+                  //       </div>
+                  //     )
+                  //   );
+                  // })
+
+                  <div>{renderPoints(obj.value)}</div>
                 )}
                 {allSectionContent.length === 1 && (
                   <div className={styles.noDataFound}>
@@ -163,25 +135,6 @@ const PDFServiceTemplate: React.FC<Iprops> = ({ documentId }) => {
           <span>No content has been developed at this time.</span>
         </div>
       )}
-      {/* <DefaultButton
-        text="PDF"
-        btnType="secondary"
-        onClick={() => {
-          const pdfTable: any =
-            document.getElementById("divToPrint")?.innerHTML;
-          debugger;
-          html2pdf()
-            .from(pdfTable)
-            .set({
-              margin: 1,
-              filename: "document.pdf",
-              image: { type: "jpeg", quality: 0.98 },
-              html2canvas: { scale: 2 },
-              jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
-            })
-            .save();
-        }}
-      /> */}
     </>
   );
 };
